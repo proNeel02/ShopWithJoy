0e251285758273270f86f39ee799d04320b4ec16
proNeel02 <prashantdabe02@gmail.com>
2023-01-26 16:42:15 +0530

    Hello

--- app.js
+++ app.js
@@ -19,9 +19,22 @@
 //     MainImg.src = smallimg[3].src;
 // }
 
+// let buy_btn = document.querySelectorAll(".buy-btn");
 
-let buy_btn = document.querySelectorAll('buy-btn');
-
-console.log(buy_btn);
+// console.log(buy_btn);
 
 
+let small_img_group = document.querySelector('.small-img-group');
+
+function productDetails(currenetElement){
+    window.location.href='sproduct.html';
+
+    // let src = currenetElement.parentElement.firstChild.getAttribute('src');
+    let src = currenetElement.getAttribute('src');
+                                                                            
+    console.log(src);
+    console.log("MainImgIn Side function ",MainImg);
+
+ var MainImg = document.getElementById("MainImg");
+ console.log("MainImgOut Side function ",MainImg);
+}
