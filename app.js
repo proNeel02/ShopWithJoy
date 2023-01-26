da393d8344a1456335edd7fc8b281ebe23a425a0
proNeel02 <prashantdabe02@gmail.com>
2023-01-26 14:14:14 +0530

    cartUpdate

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 
-var MainImg = document.getElementById("MainImg");
+// var MainImg = document.getElementById("MainImg");
 
-var smallimg = document.getElementsByClassName("small-img");
+// var smallimg = document.getElementsByClassName("small-img");
 
-smallimg[0].onclick = function(){
-    MainImg.src = smallimg[0].src;
-}
+// smallimg[0].onclick = function(){
+//     MainImg.src = smallimg[0].src;
+// }
 
-smallimg[1].onclick = function(){
-    MainImg.src = smallimg[1].src;
-}
+// smallimg[1].onclick = function(){
+//     MainImg.src = smallimg[1].src;
+// }
 
-smallimg[2].onclick = function(){
-    MainImg.src = smallimg[2].src;
-}
+// smallimg[2].onclick = function(){
+//     MainImg.src = smallimg[2].src;
+// }
 
-smallimg[3].onclick = function(){
-    MainImg.src = smallimg[3].src;
-}
+// smallimg[3].onclick = function(){
+//     MainImg.src = smallimg[3].src;
+// }
+
+
+let buy_btn = document.querySelectorAll('buy-btn');
+
+console.log(buy_btn);
+
+
